fad8ec0a6fc767c17f511cd14f7be07d7e492f64
joao21dev <joao21dev@gmail.com>
2022-03-19 10:15:41 -0300

    new comments added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import firebase from "./firebase";
+
+const useDatabase = (endpoint) => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const ref = firebase.database().ref(endpoint);
+    ref.on("value", (snapshot) => {
+      setData(snapshot.val());
+    });
+    return () => {
+      ref.off(); //limpar a conexao com o firebase (performance)
+    };
+  }, [endpoint]);
+  return data;
+};
+
+const useDatabasePush = (endpoint) => {
+  const [status, setStatus] = useState("");
+
+  const save = (data) => {
+    const ref = firebase.database().ref(endpoint);
+    ref.push(data, (err) => {
+      if (err) {
+        setStatus("ERROR");
+      } else {
+        setStatus("SUCCESS");
+      }
+    });
+  };
+  return [status, save];
+};
+const Time = ({ timestamp }) => {
+  const date = new Date(timestamp);
+  const hours = date.getHours();
+  const minutes = "0" + date.getMinutes();
+  const seconds = "0" + date.getSeconds();
+  const day = "0" + (date.getDay() + 1);
+  const month = "0" + (date.getMonth() + 1);
+  const year = date.getFullYear();
+  return `${day.substr(-2)}/${month.substr(
+    -2
+  )}/${year} ${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
+};
+const Comment = ({ comment }) => {
+  return (
+    <div>
+      {comment.content} por: {comment.user.name} em:{" "}
+      <Time timestamp={comment.createdAt} />
+    </div>
+  );
+};
+
+const Comments = () => {
+  const data = useDatabase("comments");
+
+  if (!data) {
+    return <p>Nenhum comentário foi adicionado.</p>;
+  }
+  const ids = Object.keys(data);
+  if (ids.length === 0) {
+    return <p> Carregando comentários... </p>;
+  }
+  return ids.map((id) => {
+    return <Comment key={id} comment={data[id]} />;
+  });
+};
+
+const NewComment = (props) => {
+  const [, save] = useDatabasePush("comments");
+  const [comment, setComment] = useState("");
+  const createComment = () => {
+    if (comment !== "")
+      save({
+        content: comment,
+        createdAt: firebase.database.ServerValue.TIMESTAMP,
+        user: {
+          id: "1",
+          name: "tulio",
+        },
+      });
+    setComment("");
+  };
+  return (
+    <div>
+      <textarea
+        value={comment}
+        onChange={(evt) => setComment(evt.target.value)}
+      />
+      <button onClick={createComment}>Salvar Comentário</button>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewComment />
+
+      <Comments />
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+
+var firebaseConfig = {
+
+  apiKey: "AIzaSyBJyF8ZijkFHuVyWF29iNSeijurJBK_4gA",
+
+  authDomain: "comentaki-cfc7d.firebaseapp.com",
+
+  projectId: "comentaki-cfc7d",
+
+  storageBucket: "comentaki-cfc7d.appspot.com",
+
+  messagingSenderId: "126809607075",
+
+  appId: "1:126809607075:web:0f5055603be3f413bae816"
+
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
